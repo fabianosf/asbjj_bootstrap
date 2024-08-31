8a976901e9cf74b419f4a625451c1ac827b74649
Fabiano Freitas <fabiano.freitas@gmail.com>
2024-08-31 00:31:02 -0300

    sitemap base_url

--- core/sitemaps.py
+++ core/sitemaps.py
@@ -2,11 +2,16 @@ from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
 
 class StaticViewSitemap(Sitemap):
-    priority = 0.5
     changefreq = 'daily'
+    priority = 0.5
 
     def items(self):
-        return ['index', 'sobre', 'servicos','contato','login', 'logout','register']
+        return ['home', 'sobre', 'servicos', 'contato', 'login', 'logout', 'register']
 
     def location(self, item):
         return reverse(item)
+
+    def get_urls(self, *args, **kwargs):
+        urls = super().get_urls(*args, **kwargs)
+        base_url = 'https://asbjj.com.br'
+        return [url._replace(location=base_url + url.location) for url in urls]
